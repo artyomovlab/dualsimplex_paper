d2658bd95560f68415ec8ae33a544ed77cfeddbc
Alexey Serdyukov <leshaserdyukov@gmail.com>
2024-02-01 18:25:59 +0700

    Add preprocessing

---
+++ R/setup.R
@@ -0,0 +1,5 @@
+# Install Linseed 2
+# TODO: replace with actual installation
+# TODO: ggrastr is unavailable and needs to be installed separately
+unloadNamespace("linseed2")
+devtools::load_all("../../linseed2")
