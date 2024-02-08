5cb03906ba4e5321686cde275a7d1e27734dad2a
Alexey Serdyukov <leshaserdyukov@gmail.com>
2024-02-08 17:32:16 +0300

    Add hnsc processing

--- R/setup.R
+++ R/setup.R
@@ -1,5 +1,3 @@
 # Install Linseed 2
-# TODO: replace with actual installation
-# TODO: ggrastr is unavailable and needs to be installed separately
 unloadNamespace("linseed2")
 devtools::load_all("../../linseed2")
