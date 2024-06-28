f69551212d1f9a901fbaadd54e5eb7d29499a5ae
Denis Kleverov <denklewer@gmail.com>
2024-06-27 22:14:37 -0500

    more cleaning for single cell

--- R/setup.R
+++ R/setup.R
@@ -3,3 +3,5 @@ library(devtools)
 unloadNamespace("dualsimplex")
 devtools::install_github("artyomovlab/DualSimplex")
 #devtools::load_all("../../dualsimplex") to install from local directory
+library(DualSimplex)
+options("dualsimplex-rasterize" = T)
